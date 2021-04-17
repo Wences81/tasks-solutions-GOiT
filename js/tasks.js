a7031c3657047b55a6ccaf8791d462892127becd
Wences81 <zhylinpochta@gmail.com>
2021-04-18 00:13:20 +0200

    task2

--- js/tasks.js
+++ js/tasks.js
@@ -43,3 +43,21 @@ function checkStorage(available, ordered) {
   // Пиши код выше этой строки
   return message;
 }
+
+// Task 24
+
+function getDiscount(totalSpent) {
+  const BASE_DISCOUNT = 0;
+  const BRONZE_DISCOUNT = 0.02;
+  const SILVER_DISCOUNT = 0.05;
+  const GOLD_DISCOUNT = 0.1;
+  let discount;
+  // Пиши код ниже этой строки
+  if (totalSpent >= 50000) {discount = GOLD_DISCOUNT};
+  if (totalSpent >= 20000 && totalSpent < 50000) {discount = SILVER_DISCOUNT};
+  if (totalSpent >= 5000 && totalSpent < 20000) {discount = BRONZE_DISCOUNT};
+  if (totalSpent < 5000) {discount = BASE_DISCOUNT};
+
+  // Пиши код выше этой строки
+  return discount;
+}
