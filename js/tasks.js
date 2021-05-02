179e17ef5356e34e0b88668d657dabd6d8031b97
Wences81 <zhylinpochta@gmail.com>
2021-05-03 00:16:19 +0200

    modul3/day5

--- js/tasks.js
+++ js/tasks.js
@@ -585,3 +585,222 @@ function getAllPropValues(propName) {
   
   return propValue;
 }
+
+
+
+
+// TASK 20
+
+
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  // Пиши код ниже этой строки
+ 
+ let totalPrice = 0;
+  
+  
+for (const product of products) {
+  
+  if (productName === product.name) {
+    
+   totalPrice = product.price * product.quantity;
+    
+    
+  }
+  
+}
+
+  return totalPrice;
+  // Пиши код выше этой строки
+}
+
+
+
+
+// TASK 21
+
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Пиши код ниже этой строки
+const { yesterday, today, tomorrow } = highTemperatures;
+
+
+
+// Пиши код выше этой строки
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+
+
+
+
+
+// TASK 22
+
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Пиши код ниже этой строки
+
+const {
+  yesterday,
+  today,
+  tomorrow,
+  icon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'
+} = highTemperatures;
+
+// Пиши код выше этой строки
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+
+
+
+// TASK 23
+
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Пиши код ниже этой строки
+const {
+ yesterday: highYesterday,
+  today: highToday,
+  tomorrow: highTomorrow,
+  icon: highIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+} = highTemperatures;
+
+
+// Пиши код выше этой строки
+const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+
+
+
+
+// TASK 24
+
+
+const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+];
+
+const hexColors = [];
+const rgbColors = [];
+// Пиши код ниже этой строки
+
+for (const { hex, rgb } of colors) {
+  
+  hexColors.push(hex);
+  rgbColors.push(rgb);
+}
+
+
+
+
+// TASK 25
+
+
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+// Пиши код ниже этой строки
+
+const {
+  
+  today: { low: lowToday, high: highToday, icon: todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg' },
+      
+  tomorrow: { low: lowTomorrow, high: highTomorrow, icon: tomorrowIcon =  'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg' },
+
+
+} = forecast;
+
+
+
+
+// TASK 26
+
+
+// Пиши код ниже этой строки
+function calculateMeanTemperature(forecast) {
+
+const {
+  today: { low: todayLow, high: todayHigh },
+  
+  tomorrow: { low: tomorrowLow, high: tomorrowHigh },
+  
+} = forecast;
+  // Пиши код выше этой строки
+  return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+}
+
+
+
+
+
+// TASK 27
+
+
+const scores = [89, 64, 42, 17, 93, 51, 26];
+// Пиши код ниже этой строки
+const bestScore = Math.max(...scores);
+const worstScore = Math.min(...scores);
+
+
+
+
+
+// TASK 28
+
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
+// Пиши код ниже этой строки
+const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+const bestScore = Math.max(...allScores);
+const worstScore = Math.min(...allScores);
+
+
+
+
+// TASK 29
+
+
+const defaultSettings = {
+  theme: 'light',
+  public: true,
+  withPassword: false,
+  minNumberOfQuestions: 10,
+  timePerQuestion: 60,
+};
+const overrideSettings = {
+  public: false,
+  withPassword: true,
+  timePerQuestion: 30,
+};
+// Пиши код ниже этой строки
+const finalSettings = {...defaultSettings, ...overrideSettings};
