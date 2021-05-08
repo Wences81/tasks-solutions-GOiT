9f85abc6847c910925913cd0f4cf45771ad3706e
Wences81 <zhylinpochta@gmail.com>
2021-05-08 22:58:46 +0200

    modul4/day2

--- js/tasks.js
+++ js/tasks.js
@@ -1108,3 +1108,63 @@ function makeMessage(pizzaName, callback) {
   
   return callback(pizzaName);
 }
+
+
+
+
+
+// TASK 3
+
+
+function makePizza(pizzaName, callback) {
+  console.log(`Пицца ${pizzaName} готовится, ожидайте...`);
+  callback(pizzaName);
+}
+
+makePizza('Роял гранд', function deliverPizza(pizzaName) {
+  console.log(`Доставляем пиццу ${pizzaName}.`);
+});
+// Пиши код ниже этой строки
+
+makePizza('Ультрасыр', function eatPizza(pizzaName) {
+          
+   console.log(`Едим пиццу ${pizzaName}.`);
+});
+
+
+
+
+
+// TASK 4
+
+
+const pizzaPalace = {
+  pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+  order(pizzaName, onSuccess, onError) {
+    
+  if (!this.pizzas.includes(pizzaName)) {
+    
+    return onOrderError(`В ассортименте нет пиццы с названием ${pizzaName}.`);
+    
+     
+  }
+    return makePizza(pizzaName);
+  },
+};
+// Пиши код выше этой строки
+
+// Колбэк для onSuccess
+function makePizza(pizzaName) {
+  return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+}
+
+// Колбэк для onError
+function onOrderError(error) {
+  return `Ошибка! ${error}`;
+}
+
+// Вызовы метода с колбэками
+pizzaPalace.order('Аль Копчино', makePizza, onOrderError);
+pizzaPalace.order('Четыре нарезона', makePizza, onOrderError);
+pizzaPalace.order('Биг майк', makePizza, onOrderError);
+pizzaPalace.order('Венская', makePizza, onOrderError);
