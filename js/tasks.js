b3fc3aee7546d7566283bf64434572ba1a75a960
Wences81 <zhylinpochta@gmail.com>
2021-05-13 23:12:25 +0200

    modul5/day1

--- js/tasks.js
+++ js/tasks.js
@@ -1363,3 +1363,114 @@ console.log(secondInvoke);
 // Почта poly@hotmail.de удалена из рассылки.
 
 console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
+
+
+
+
+
+
+
+
+
+
+// MODUL 5
+
+
+
+
+// TASK 1
+
+
+const parent = {
+  name: 'Stacey',
+  surname: 'Moore',
+  age: 54,
+  heritage: 'Irish'
+};
+// Пиши код ниже этой строки
+
+const child = Object.create(parent);
+
+// Пиши код выше этой строки
+child.name = 'Jason';
+child.age = 27;
+
+
+
+
+
+
+// TASK 2
+
+
+
+const ancestor = {
+  name: 'Paul',
+  age: 83,
+  surname: 'Dawson',
+  heritage: 'Irish'
+};
+// Пиши код ниже этой строки
+
+const parent = Object.create(ancestor);
+parent.name = 'Stacey';
+parent.surname = 'Moore';
+parent.age = 54;
+
+const child = Object.create(parent);
+child.name = 'Jason';
+child.age = 27;
+
+// Пиши код выше этой строки
+
+
+
+
+
+// TASK 5
+
+
+
+function Car({ brand, model, price }) {
+  this.brand = brand;
+  this.model = model;
+  this.price = price;
+}
+Car.prototype.getPrice = function () {
+  return this.price;
+};
+
+Car.prototype.changePrice = function (newPrice) {
+  this.price = newPrice;
+};
+
+
+
+
+
+// TASK 6
+
+
+
+function Storage(items) {
+  this.items = items;
+}
+
+Storage.prototype.getItems = function() {
+  return this.items;
+};
+Storage.prototype.addItem = function(newItem) {
+  this.items.push(newItem);
+};
+Storage.prototype.removeItem = function(item) {
+  this.items.splice(1,1);
+};
+
+
+// Пиши код выше этой строки
+const storage = new Storage(['Нанитоиды', 'Пролонгер', 'Антигравитатор']);
+console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+storage.addItem('Дроид');
+console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+storage.removeItem('Пролонгер');
+console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
