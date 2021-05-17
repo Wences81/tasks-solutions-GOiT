f6dbc7b07d36015c04a4afc3059b050d96797000
Wences81 <zhylinpochta@gmail.com>
2021-05-17 23:33:39 +0200

    modul5/day2

--- js/tasks.js
+++ js/tasks.js
@@ -1474,3 +1474,39 @@ storage.addItem('Дроид');
 console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
 storage.removeItem('Пролонгер');
 console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+
+
+
+
+
+
+// TASK 7
+
+
+
+
+function StringBuilder(baseValue) {
+  this.value = baseValue;
+}
+StringBuilder.prototype.getValue = function() {
+  return this.value;
+}
+StringBuilder.prototype.padEnd = function(str) {
+  this.value += str;
+}
+
+StringBuilder.prototype.padStart = function(str) {
+  this.value = str + this.value;
+}
+StringBuilder.prototype.padBoth = function(str) {
+  this.value = str + this.value + str;
+}
+// Пиши код выше этой строки
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // '.'
+builder.padStart('^');
+console.log(builder.getValue()); // '^.'
+builder.padEnd('^');
+console.log(builder.getValue()); // '^.^'
+builder.padBoth('=');
+console.log(builder.getValue()); // '=^.^='
