f4cceba2f93a12358875e03c4bb0da88ffd06b08
Wences81 <zhylinpochta@gmail.com>
2021-05-20 19:51:42 +0200

    modul5/day5

--- js/tasks.js
+++ js/tasks.js
@@ -1787,3 +1787,148 @@ const bmw = new Car({ price: 64000 });
 
 console.log(Car.checkPrice(audi.price)); // Всё хорошо, цена в порядке.
 console.log(Car.checkPrice(bmw.price)); // Внимание! Цена превышает допустимую.
+
+
+
+
+
+
+
+// TASK 17
+
+
+
+
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+// Пиши код ниже этой строки
+class Admin extends User {
+  static AccessLevel = { BASIC: 'basic', SUPERUSER: 'superuser' }
+}
+
+
+
+
+
+
+
+// TASK 18
+
+
+
+
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+
+class Admin extends User {
+  // Пиши код ниже этой строки
+  accessLevel;
+  
+  constructor({email, accessLevel}) {
+    super(email);
+    this.accessLevel = accessLevel;
+  }
+    
+  static AccessLevel = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser'
+  };
+
+  // Пиши код выше этой строки
+}
+
+const mango = new Admin({
+  email: 'mango@mail.com',
+  accessLevel: Admin.AccessLevel.SUPERUSER
+});
+
+console.log(mango.email); // mango@mail.com
+console.log(mango.accessLevel); // superuser
+
+
+
+
+
+
+// TASK 19
+
+
+
+
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
+  // Пиши код ниже этой строки
+
+  static AccessLevel = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser'
+  };
+
+  accessLevel;
+  blacklistedEmails = [];
+
+  constructor({ email, accessLevel }) {
+    super(email);
+    this.accessLevel = accessLevel;
+  }
+  blacklist(email) {
+    this.blacklistedEmails.push(email);
+  }
+  
+  isBlacklisted(email) {
+    return this.blacklistedEmails.includes(email);
+  }
+  // Пиши код выше этой строки
+}
+
+const mango = new Admin({
+  email: 'mango@mail.com',
+  accessLevel: Admin.AccessLevel.SUPERUSER
+});
+
+console.log(mango.email); // mango@mail.com
+console.log(mango.accessLevel); // superuser
+mango.blacklist('poly@mail.com');
+console.log(mango.blacklistedEmails); // 'poly@mail.com'
+console.log(mango.isBlacklisted('mango@mail.com')); //  false
+console.log(mango.isBlacklisted('poly@mail.com')); // true
